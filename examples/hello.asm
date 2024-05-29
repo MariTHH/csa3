26481dfddfeb88fc2d3ad203edb9722832298cf5
Maria Tolstykh <118300117+MariTHH@users.noreply.github.com>
2024-05-29 13:00:31 +0300

    Update hello.asm

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,11 +1,10 @@
 .data:
-    num str_len: 12
-    str hw: 12, 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'
+    str hw: 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'
     num cur_ind: 0
 
 .text:
       mov [hw] cur_ind
-      add cur_ind 1
+      add cur_ind 0
 loop: out# cur_ind
       cmp* cur_ind 0
       je end
