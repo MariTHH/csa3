92f7fa1dc9ae987f9e6b4ea19186f5288e650792
Maria Tolstykh <118300117+MariTHH@users.noreply.github.com>
2024-05-29 13:01:12 +0300

    Update hello_user.asm

--- examples/hello_user.asm
+++ examples/hello_user.asm
@@ -1,13 +1,13 @@
 .data:
-    str prompt: 18, 'W', 'h', 'a', 't', ' ', 'i', 's', ' ', 'y', 'o', 'u', 'r', ' ', 'n', 'a', 'm', 'e', '?', ' '
-    str greeting: 7, 'H', 'e', 'l', 'l', 'o', ',', ' '
+    str prompt: 'W', 'h', 'a', 't', ' ', 'i', 's', ' ', 'y', 'o', 'u', 'r', ' ', 'n', 'a', 'm', 'e', '?', ' '
+    str greeting: 'H', 'e', 'l', 'l', 'o', ',', ' '
     num name: 0
     num nameCounter: 0
     num cur_ind: 0
 
 .text:
       mov [prompt] cur_ind
-      add cur_ind 1
+      add cur_ind 0
 ask: out# cur_ind
           cmp* cur_ind 0
           je greet
@@ -15,15 +15,11 @@ ask: out# cur_ind
 greet: mov [greeting] cur_ind
 loop: out# cur_ind
        cmp* cur_ind 0
-       je name_count
+       je naming
        jmp loop
-name_count: in name
-            save nameCounter
+
 naming: in name
         out_char name
-        add nameCounter -1
-        cmp 0 nameCounter
-        je end
         jmp naming
 
 end:    hlt
